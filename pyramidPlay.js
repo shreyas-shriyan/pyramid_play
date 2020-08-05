0b68cb01af90be7143f07298efe443cdb5910959
vatsrahul79 <vatsrahul79@gmail.com>
2020-08-05 19:01:06 +0530

    pyramidPlay

--- pyramidPlay.js
+++ pyramidPlay.js
@@ -5,13 +5,16 @@ const zero = num => new Array(num).fill("0").join(".")
 /* console.log(dots(4))
 console.log(zero(4)) */
 
-const pyramid = size =>{
+const pattern = (size,offset = 0) =>{
     let output = []
-    for(let i=0;i<size;i++){
+    for(let i=0;i<size-offset;i++){
         output[i] = dots(size-i)+zero(i+1)+dots(size-i)
     }
-    return output.join("\n")
+    return output
 }
 
+const Inverse_pyramid = size =>pattern(size).reverse().join('\n')
 
-console.log(pyramid(4))
+const rhombus = size =>[...pattern(size,1),...pattern(size).reverse()].join('\n')
+
+console.log(rhombus(4))
